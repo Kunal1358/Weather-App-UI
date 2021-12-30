85a4361493d2eb80cd80112bb2cf5611f8d9de4c
Kunal1358 <kunalkohli8054@.com>
2021-12-30 09:16:34 +0530

    Output printed on screen

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,24 @@ console.log('Hello from server side');
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
 
+    messageOne.textContent = 'Loading weather...'
+    messageTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.address)
-                console.log(data)
+                messageOne.textContent = ''
+                messageTwo.textContent = data.location
             }
         })
 
